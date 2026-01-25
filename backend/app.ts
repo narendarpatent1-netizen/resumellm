304f9a17f6b0545f4fd59d50e895f347d05583fc
puttanarendar <narenderp1431@gmail.com>
2026-01-25 18:12:41 +0530

    new commit code

--- backend/app.ts
+++ backend/app.ts
@@ -6,6 +6,8 @@ import interviewRoutes from "./routes/interview.routes";
 import AuthRoutes from "./routes/auth.routes";
 import cookieParser from "cookie-parser";
 import compression from "compression";
+import rateLimit from 'express-rate-limit';
+import helmet from "helmet";
 
 dotenv.config();
 
@@ -16,15 +18,32 @@ app.use(cors({
     origin: ["http://localhost:8080", "http://localhost:4000"],
     credentials: true
 }));
+app.use(helmet());
+const perApiLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 15, // 15 requests
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    status: 429,
+    error: "Too many requests for this API. Try again after 1 minute."
+  },
+
+  // 🔑 KEY PART (this makes it PER API)
+  keyGenerator: (req) => {
+    return `${req.ip}-${req.path}`;
+  }
+});
+// ✅ Apply to ALL APIs
+app.use('/api', perApiLimiter);
 app.use(express.json());
 app.use(cookieParser());
 app.use(compression());
 
 app.use("/api/interview", interviewRoutes);
 app.use('/api/auth', AuthRoutes);
-
 connectDB();
 
 app.listen(4000, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running on port 4000");
 });
--- backend/models/User.ts
+++ backend/models/User.ts
@@ -18,22 +18,11 @@ const userSchema = new mongoose.Schema<IUser>(
         name: { type: String, required: true, trim: true, minlength: 2, maxlength: 10, unique: true },
         email: { type: String, required: true, unique: true, lowercase: true, trim: true },
         password: { type: String, required: true, minlength: 6, select: false },
-        refreshToken: { type: String, required: true },
+        refreshToken: { type: String },
         isActive: { type: Boolean, default: true },
         lastLoginAt: { type: Date }
     },
     { timestamps: true }
 );
-
-// 🔐 Pre-save hook
-userSchema.pre("save", async function () {
-    if (!this.isModified("password")) return;
-    this.password = await bcrypt.hash(this.password, 10);
-});
-
-// 🔎 Compare password
-userSchema.methods.comparePassword = function (password: string) {
-    return bcrypt.compare(password, this.password);
-};
 
 export default mongoose.model<IUser>("User", userSchema);
